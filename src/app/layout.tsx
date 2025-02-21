4563cb05ff621e5b9f85c961ce07e38b2a89a2ff
Arhaan Keshwani <arhaankeshwani200@gmail.com>
2025-02-21 01:59:32 -0500

    testing analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/react"
+
 
 export const metadata: Metadata = {
   title: "Arhaan Keshwani",
@@ -15,6 +17,7 @@ export default function RootLayout({
     <html lang="en">
       <body className="antialiased">
         {children}
+        <Analytics />
       </body>
     </html>
   );
